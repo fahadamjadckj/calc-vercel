d0a984ea58d9548e4b4fddea36760da7a3e1663d
martialdwarf <mohammadfahadamajd@gmail.com>
2022-08-20 02:12:20 -0700

    first commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const puppeteer = require("puppeteer-core");
+const chrome = require("chrome-aws-lambda");
+
+export default async function handler(req, res) {
+  const browser = await puppeteer.launch({
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  });
+
+  const page = await browser.newPage();
+  await page.goto("https://www.wikipedia.com");
+  let title = await page.title();
+  res.status(200).json({ name: "title" });
 }
