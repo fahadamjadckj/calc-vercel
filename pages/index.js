47a851a8a8a5a38933b395e9e13a4a1424155edd
martialdwarf <mohammadfahadamajd@gmail.com>
2022-08-20 13:43:55 -0700

    some 2

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
   const [error, setError] = useState(null);
 
   const invokeError = (err) => {
-    setStatus(null);
+    // setStatus(null);
     setError(err);
   };
 
