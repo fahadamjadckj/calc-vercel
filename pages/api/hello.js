990d0fcedccf7ce1700e26f068ea189b764dd238
martialdwarf <mohammadfahadamajd@gmail.com>
2022-08-20 03:50:04 -0700

    returnss

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,29 +4,12 @@ const puppeteer = require("puppeteer-core");
 const chrome = require("chrome-aws-lambda");
 
 export default async function handler(req, res) {
-  const data = await getData();
-  res.json({ data });
-}
-
-async function getData() {
   const browser = await puppeteer.launch({
     args: chrome.args,
     executablePath: await chrome.executablePath,
     headless: chrome.headless,
   });
-
-  const page = await browser.newPage();
   await page.goto("http://lms.uaf.edu.pk/login/index.php");
-  let regNumber = "2020-ag-8322";
-  await page.type("#REG", regNumber);
-  await Promise.all([
-    page.click("input[value=Result]"),
-    page.waitForNavigation(),
-  ]);
-
   let title = await page.title();
-
-  await browser.close();
-
-  return title;
+  res.status(200).json({ text: title });
 }
