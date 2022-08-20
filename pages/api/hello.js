7919600058a5cb1e1ae8b19c2bb16e808a61b477
martialdwarf <mohammadfahadamajd@gmail.com>
2022-08-20 02:46:10 -0700

    first working of gpa calc

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -12,29 +12,30 @@ export default async function handler(req, res) {
 
   const page = await browser.newPage();
   await page.goto("http://lms.uaf.edu.pk/login/index.php");
-
   await page.type("#REG", regNumber);
-
   await Promise.all([
     page.click("input[value=Result]"),
     page.waitForNavigation(),
   ]);
 
   let data = await page.evaluate(() => {
-    let values = []
-    let tableRows = document.querySelectorAll('tr')
+    let values = [];
+    let tableRows = document.querySelectorAll("tr");
 
     tableRows.forEach((row) => {
-      let children = {}
-      let index = 0
+      let children = {};
+      let index = 0;
       row.childNodes.forEach((child) => {
         if (child.innerText != null) {
-          children[index] = child.innerText
-          index++
+          children[index] = child.innerText;
+          index++;
         }
-      })
-      values.push(children)
-    })
+      });
+      values.push(children);
+    });
 
-  res.status(200).json({ name: JSON.stringify(values) });
+    return values;
+  });
+
+  res.status(200).json({ name: JSON.stringify(data) });
 }
