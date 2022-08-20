2afb1778284517650fe5c93b4b93538300ae6b2d
martialdwarf <mohammadfahadamajd@gmail.com>
2022-08-20 03:54:39 -0700

    returnss 2

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -11,6 +11,14 @@ export default async function handler(req, res) {
   });
   let page = await browser.newPage();
   await page.goto("http://lms.uaf.edu.pk/login/index.php");
+
+  await page.type("#REG", "2020-ag-8322");
+
+  await Promise.all([
+    page.click("input[value=Result]"),
+    page.waitForNavigation(),
+  ]);
+
   let title = await page.title();
   res.status(200).json({ text: title });
 }
